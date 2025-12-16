b6df122b102c516e13942ea0f9a18ae36943f5cd
A60874022 <a60874022@yandex.ru>
2025-12-16 20:54:49 +0500

    деплой

--- ad_service/settings.py
+++ ad_service/settings.py
@@ -124,9 +124,13 @@ AUTHENTICATION_BACKENDS = [
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
